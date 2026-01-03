919e03617eb39159fad9bc3ff5412a9fa28951f2
WaqarHassan20 <waqarkhalid2024@gmail.com>
2026-01-03 19:24:26 +0500

    python syntax and know how

---
+++ python/13_async_python/06_bg_worker.py
@@ -0,0 +1,19 @@
+import asyncio
+import threading
+import time
+
+
+def background_worker():
+    while True:
+        time.sleep(1)
+        print("Logging the system health ...")
+
+
+async def fetch_orders():
+    await asyncio.sleep(2)
+    print("Fetched orders.")
+
+
+thread = threading.Thread(target=background_worker, daemon=True).start()
+
+asyncio.run(fetch_orders())
