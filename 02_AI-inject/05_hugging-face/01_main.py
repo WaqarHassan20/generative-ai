0a81ec62a01af1085028f080c1b0ba1ef4fd08bd
waqarhassan20 <waqarkhalid2024@gmail.com>
2026-01-26 20:24:02 +0500

    sample code snippet added

---
+++ 02_AI-inject/05_hugging-face/01_main.py
@@ -0,0 +1,18 @@
+from transformers import pipeline
+
+pipe = pipeline("image-text-to-text", model="google/gemma-3-4b-it")
+
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "image",
+                "url": "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/p-blog/candy.JPG",
+            },
+            {"type": "text", "text": "What animal is on the candy?"},
+        ],
+    }
+]
+
+# Note : you have to install the torch library to run this code snippet. 
